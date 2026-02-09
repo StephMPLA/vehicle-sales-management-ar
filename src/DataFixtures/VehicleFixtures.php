377f15d3a570589765ff57197d68078b7718afe9
StephMPLA <stephmpla@gmail.com>
2026-02-09 17:31:09 +0100

    fix fixtures

--- src/DataFixtures/VehicleFixtures.php
+++ src/DataFixtures/VehicleFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use App\Entity\Brand;
 use App\Entity\Vehicle;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class VehicleFixtures extends Fixture
+class VehicleFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -16,5 +17,13 @@ class VehicleFixtures extends Fixture
         $alfa->setBrand($this->getReference(BrandFixtures::BRAND_ALFA_REFERENCE, Brand::class));
 
         //Todo à termier !!
+
+
+    }
+    public function getDependencies(): array
+    {
+        return [
+            BrandFixtures::class
+        ];
     }
 }
