bbc5ed1eeaad4dc80a2190793155fee0c6358722
StephMPLA <LeM@08072010>
2026-02-23 15:46:35 +0100

    feat: implement admin client profile page with contact actions

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+class AdminUserController extends AbstractController
+{
+    #[Route('/admin/user/{id}', name: 'admin_user_show')]
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user/show.html.twig', [
+            'client' => $user,
+        ]);
+    }
+}
